3cfc9644a03223e358c6f9015c73917e78cadad6
Mohansinghpawar <88230569+Mohansinghpawar@users.noreply.github.com>
2022-09-06 22:13:11 +0530

    Create CreateStatement.java

---
+++ CreateStatement.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExample {
+   static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+   static final String USER = "SYSTEM";
+   static final String PASS = "root";
+
+   public static void main(String[] args) {
+      // Open a connection
+      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+         Statement stmt = conn.createStatement();
+      ) {		      
+         String sql = "CREATE DATABASE STUDENTS";
+         stmt.executeUpdate(sql);
+         System.out.println("Database created successfully...");   	  
+      } catch (SQLException e) {
+         e.printStackTrace();
+      } 
+   }
+}
